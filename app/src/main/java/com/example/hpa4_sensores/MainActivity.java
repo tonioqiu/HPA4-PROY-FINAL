8f8a313f89c6181e8cd1055b181c73b08b9ba2b7
tonioqiu <tonioqiu.dev@gmail.com>
2022-12-07 21:47:27 -0500

    Added Button and Intent
    added Buttons on main navigation screen and intent to NFC Activity.

--- app/src/main/java/com/example/hpa4_sensores/MainActivity.java
+++ app/src/main/java/com/example/hpa4_sensores/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.hpa4_sensores;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button NFCButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        NFCButton = (Button)findViewById(R.id.btnNFC);
+
+        NFCButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent nfcintent = new Intent (MainActivity.this ,NFCActivity.class);
+                startActivity(nfcintent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/hpa4_sensores/NFCActivity.java
@@ -0,0 +1,21 @@
+package com.example.hpa4_sensores;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class NFCActivity extends AppCompatActivity {
+    Button NFCButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nfcactivity2);
+
+
+
+    }
+}
