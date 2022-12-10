b4762a20ccd548244357a5c9469216bda6bbd786
Fernando Zeng <fernando.zheng@gmail.com>
2022-12-10 15:26:34 -0500

    Parte de huella agregado

---
+++ app/src/main/java/com/example/hpa4_sensores/FingerPrintActivity.java
@@ -0,0 +1,14 @@
+package com.example.hpa4_sensores;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FingerPrintActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_finger_print2);
+    }
+}
