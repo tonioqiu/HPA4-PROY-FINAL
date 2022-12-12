45c408ce203d9331caaebb96a79864a8d8bb6b50
Carlos Tuñón <ctunon@ayudinga.org>
2022-12-11 20:19:36 -0500

    Update MainActivity - Boton Sensor 3

--- app/src/main/java/com/example/hpa4_sensores/MainActivity.java
+++ app/src/main/java/com/example/hpa4_sensores/MainActivity.java
@@ -8,19 +8,28 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    Button NFCButton;
+    Button NFCButton, VoiceButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         NFCButton = (Button)findViewById(R.id.btnNFC);
+        VoiceButton = (Button)findViewById(R.id.btnVoice);
 
         NFCButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent nfcintent = new Intent (MainActivity.this ,NFCActivity.class);
                 startActivity(nfcintent);
+            }
+        });
+
+        VoiceButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent voiceintent = new Intent (MainActivity.this ,VoiceActivity.class);
+                startActivity(voiceintent);
             }
         });
     }
