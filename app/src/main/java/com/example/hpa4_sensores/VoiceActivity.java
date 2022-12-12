45666cfc379f6a8a100567ad154b08c98da94efd
Carlos Tuñón <ctunon@ayudinga.org>
2022-12-11 22:31:34 -0500

    Voice Activity - permissions, .java, .xml v1

--- app/src/main/java/com/example/hpa4_sensores/VoiceActivity.java
+++ app/src/main/java/com/example/hpa4_sensores/VoiceActivity.java
@@ -1,14 +1,138 @@
 package com.example.hpa4_sensores;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class VoiceActivity extends AppCompatActivity {
 
+    int count;
+    ImageButton imageButton;
+    EditText editText;
+
+
+    SpeechRecognizer speechRecognizer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_voice);
+
+        imageButton=findViewById(R.id.btnMic);
+        editText=findViewById(R.id.edtMic);
+
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED){
+
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.RECORD_AUDIO}, 1);
+
+        }
+
+        speechRecognizer=SpeechRecognizer.createSpeechRecognizer(this);
+
+        Intent speechRecognizerIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if(count==0){
+
+                    speechRecognizer.startListening(speechRecognizerIntent);
+                count=1;
+                }
+
+                else{
+                    speechRecognizer.stopListening();
+                    count=0;
+                }
+            }
+        });
+
+        speechRecognizer.setRecognitionListener(new RecognitionListener() {
+            @Override
+            public void onReadyForSpeech(Bundle bundle) {
+
+            }
+
+            @Override
+            public void onBeginningOfSpeech() {
+
+            }
+
+            @Override
+            public void onRmsChanged(float v) {
+
+            }
+
+            @Override
+            public void onBufferReceived(byte[] bytes) {
+
+            }
+
+            @Override
+            public void onEndOfSpeech() {
+
+            }
+
+            @Override
+            public void onError(int i) {
+
+            }
+
+            @Override
+            public void onResults(Bundle results) {
+
+                ArrayList<String> data = results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+
+                editText.setText(data.get(0));
+
+            }
+
+            @Override
+            public void onPartialResults(Bundle bundle) {
+
+            }
+
+            @Override
+            public void onEvent(int i, Bundle bundle) {
+
+            }
+        });
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if(requestCode==1){
+
+            if(grantResults[0]==PackageManager.PERMISSION_GRANTED){
+
+                Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT);
+
+            }
+
+            else{
+                Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT);
+
+            }
+
+        }
+
     }
 }
